32d22288611f2c8771ff2533ea29c9d3f83805d6
Evan Summers <evan.summers@wandelbots.com>
2026-06-02 13:34:54 +0200

    tsnapi only works for ESM build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,14 +2,24 @@
 import { defineConfig } from "tsdown"
 import ApiSnapshot from "tsnapi/rolldown"
 
-export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/lib/deprecated/v1/index.ts",
-    "src/lib/v2/index.ts",
-  ],
-  format: ["esm", "cjs"],
-  clean: true,
-  sourcemap: true,
-  plugins: [ApiSnapshot({ update: !process.env.CI })],
-})
+const entry = [
+  "src/index.ts",
+  "src/lib/deprecated/v1/index.ts",
+  "src/lib/v2/index.ts",
+]
+
+export default defineConfig([
+  {
+    entry,
+    format: "esm",
+    clean: true,
+    sourcemap: true,
+    plugins: [ApiSnapshot({ update: !process.env.CI })],
+  },
+  {
+    entry,
+    format: "cjs",
+    clean: true,
+    sourcemap: true,
+  },
+])
